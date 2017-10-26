860e36bbdf018967c426f1149fa4f9fef895e302
unknown <hoangtkc@L02052MBE202.campus.aston.ac.uk>
2017-10-26 16:08:37 +0100

    Generate route function

--- CS3910 - Python/Main.py
+++ CS3910 - Python/Main.py
@@ -1,4 +1,4 @@
-import Graph, OrderedSet
+import Graph, OrderedSet, itertools
 from collections import deque
 
 cities = Graph.Graph()
@@ -27,11 +27,35 @@ route.add('D')
 print(route.__len__())
 
 def getRouteCost(cities,route):
-
     routeCost = 0
     while route.__len__() > 1:
              routeCost += cities.get_weight(route.pop(), route.peek())
     return routeCost
 
+
 print(getRouteCost(cities,route))
 
+def someRandomRoutes(cities):
+    return itertools.combinations(cities.vertices, 4)
+
+
+
+def combinations(iterable, r):
+    # combinations('ABCD', 2) --> AB AC AD BC BD CD
+    # combinations(range(4), 3) --> 012 013 023 123
+    pool = tuple(iterable)
+    n = len(pool)
+    if r > n:
+        return
+    indices = list(range(r))
+    yield tuple(pool[i] for i in indices)
+    while True:
+        for i in reversed(range(r)):
+            if indices[i] != i + n - r:
+                break
+        else:
+            return
+        indices[i] += 1
+        for j in range(i+1, r):
+            indices[j] = indices[j-1] + 1
+        yield tuple(pool[i] for i in indices)
