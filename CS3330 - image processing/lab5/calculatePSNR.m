3bef68480f154517e19f756d3a1f5ad30c91aed7
aConLaptop <hoangtkc95@gmail.com>
2017-11-20 18:48:21 +0000

    add comments

--- CS3330 - image processing/lab5/calculatePSNR.m
+++ CS3330 - image processing/lab5/calculatePSNR.m
@@ -5,7 +5,7 @@ function [ PSNR ] = calculatePSNR( img1,img2 )
 diff = double(img1) - double(img2);
 diff = diff.^2;
 diff = sum(diff(:));
-MSE = diff./(size(img1,1)*size(img1,2));
+MSE = diff./(size(img1,1)*size(img1,2)); % 256*256
 PSNR = 10*log10(255.^2./MSE);
 
 end
