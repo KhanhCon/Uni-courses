b461834001ce08f16a15cf4e91c56840d4b85e83
ConAston <hoangtkc95@gmail.com>
2017-11-21 10:53:08 +0000

    update point detectionafter lab assessment 2

--- CS3330 - image processing/feature detection/line_detection.m
+++ CS3330 - image processing/feature detection/line_detection.m
@@ -1,9 +1,9 @@
-function [ line_detected_img ] = line_detection( img )
+function [ line_detected_img ] = line_detection( img,threshold )
 %LINE_DETECTION Summary of this function goes here
 %   Detailed explanation goes here
 mask = [2,-1,-1;-1,2,-1;-1,-1,2];
 line_detected_img = filter2(mask,rgb2gray(img));
-line_detected_img(line_detected_img<240) = 0;
+line_detected_img(line_detected_img<threshold) = 0;
 figure,image(line_detected_img),axis off,colormap gray(256),title('line detection');
 
 end
--- CS3330 - image processing/feature detection/point_detection.m
+++ CS3330 - image processing/feature detection/point_detection.m
@@ -1,9 +1,10 @@
-function [ point_detected_img ] = point_detection( img )
+function [ point_detected_img ] = point_detection( img,threshold )
 %POINT_DETECTION Summary of this function goes here
 %   Detailed explanation goes here
 mask = [-1,-1,-1;-1,8,-1;-1,-1,-1];
 point_detected_img = filter2(mask,rgb2gray(img));
-%point_detected_img(point_detected_img<200) = 0;
+point_detected_img(point_detected_img<threshold) = 0;
+point_detected_img(point_detected_img>threshold) = 1;
 figure,image(point_detected_img),axis off,colormap gray(256),title('point detection');
 
 end
