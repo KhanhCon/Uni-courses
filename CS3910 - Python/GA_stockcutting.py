ce1417b63c930b98f5bc927601b7ad9f31db6529
aConLaptop <hoangtkc95@gmail.com>
2017-11-25 01:26:09 +0000

    single chromosome

--- CS3910 - Python/GA_stockcutting.py
+++ CS3910 - Python/GA_stockcutting.py
@@ -1,9 +1,9 @@
-
 import bisect
 
 
-class Chromosome:
+class SolutionChromosome:
     def __init__(self):
+        chromosome = []
         age = 0
         fitness = self.__evaluate()
 
@@ -14,3 +14,24 @@ class Chromosome:
         return 0
 
 
+class PatternChromosome:
+    PRICE = None
+
+    def __init__(self):
+        pattern = []
+        fitness = self.__evaluate()
+
+    def __evaluate(self, penalty):
+        waste = self.waste
+        return PatternChromosome.PRICE[self.stock_size] + waste * penalty if waste >= 0 else 99999
+
+    def __update_waste(self):
+        self.pattern[-1] = self.pattern[0] - sum(self.pattern[1:-1])
+
+    @property
+    def stock_size(self):
+        return self.pattern[0]
+
+    @property
+    def waste(self):
+        return self.pattern[-1]
