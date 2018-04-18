2a7789a497127cec903a01dc4da1ac1a231d04ec
aConLaptop <hoangtkc95@gmail.com>
2018-04-18 09:34:00 +0100

    Page rank

--- CS3320 - Information retrieval/coursework 2 - page rank/PageRank.py
+++ CS3320 - Information retrieval/coursework 2 - page rank/PageRank.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 
+#This function takes a set of edges then return all the nodes
 def get_node(edges):
     nodes = set()
     for edge in edges:
@@ -9,46 +10,43 @@ def get_node(edges):
     return nodes
 
 def random_walk(edges, tele_prob, iters):
-    # Max iteration 10000
     nodes = get_node(edges)
     histogram = {}
     for node in nodes:
         histogram[node] = 0
-    currentNode = 0  # 5 nodes
+    currentNode = 0 #Set current node to 0 (first node)
     for i in range(iters):
-        histogram[currentNode] += 1.0
-
+        histogram[currentNode] += 1.0 #add 1.0 to current node's score
         prob = random.uniform(0, 1)
-        if prob < tele_prob:
-            # print("tele")
+        if prob < tele_prob: # Perform teleport
             currentNode = random.sample(nodes, 1)[0]
         else:
-            # print("walk")
-            links = []
+            links = [] # All the out-links a node contains
             for edge in edges:
                 if edge[0] == currentNode:
                     links.append(edge)
-            if not links:
-                # continue
+            if not links: #If a node has no out-links then perform teleport
                 currentNode = random.sample(nodes,1)[0] # teleport when stuck
             else:
+                # Jump to a one of the node that current node links to
                 currentNode = random.choice(links)[1]
+
+    #Compute a histogram with nodes's score
     for node, freq in histogram.iteritems():
         histogram[node] = round(freq / iters, 2)
     return histogram
 
+#This function takes a set of edges then return a transition matrix
 def transition_matrix(edges):
-    # Function return initial matrix
-    nodes = get_node(edges)
-    matrix = np.zeros((len(nodes), len(nodes)))
-    # matrix = {}
+
+    nodes = get_node(edges) # Get a set of nodes
+    matrix = np.zeros((len(nodes), len(nodes))) #Initialise a zeros matrix
 
     for node in nodes:
-        # matrix[node] = [0.0]*len(nodes)
-        links = []
+        links = [] #out-links of a node
         for edge in edges:
             if node == edge[0]:
-                links.append(edge[1])#
+                links.append(edge[1])
 
         for i in nodes:
             matrix[node][i] = round(1.0/len(links),2) if i in links else 0
@@ -56,7 +54,9 @@ def transition_matrix(edges):
     return matrix, nodes
 
 def pagerank(edges, iters, tele_prob = 0):
+    #Transition matrix
     matrixP, nodes = transition_matrix(edges)
+    # an arbitrary probability vector (1/Ns where N is the number of all states)
     vector = np.array([1.0/len(nodes)]*len(nodes))
     for i in range(iters):
         vector = vector.dot(matrixP) #Mulitply matrix and vector
@@ -64,10 +64,22 @@ def pagerank(edges, iters, tele_prob = 0):
 
 
 if __name__ == "__main__":
-    edges = {(0, 1), (1, 2), (2, 0), (2, 2), (2, 3), (3, 3), (3, 4)}
-    HI = random_walk(edges, 0.1, 45000)
-    print(HI)
-    print(pagerank(edges, 5000))
+    edges = {(0, 1), (1, 2), (2, 0), (2, 2), (2, 3), (3, 3)}
 
-#Question 3: pagerank requirese less
-#Question 4: No you cant increase score by putting more links
+    print(random_walk(edges, 0.1, 45000))
+    print(pagerank(edges, 1000))
+    print(pagerank(edges, 2000))
+    print(pagerank(edges, 5000))
+    print(pagerank(edges, 6000))
+
+    #In crease outlinks from node 1.
+    edges_increase_out = {(0, 1), (1, 0), (1, 2),(1,3), (2, 0), (2, 2), (2, 3), (3, 3)}
+
+    print("Increase outlinks from node 1: %s" %random_walk(edges_increase_out, 0.1, 100000))
+
+
+    #Question 3: As we can see above, Pagerank requires significantly less iterration than randomwalk
+    # in order to reach steady state ( 45000 compares to 5000)
+
+    #Question 4: No you cant influence score by chaning the number of out-links it contains.
+    # The score for node 1 stays the same when we put more out-links into it.
